e050d0f9c9f82a3de4cbb92a7aba533644147d2d
Alex Hart <alex@hartcode.com>
2017-08-28 10:40:35 -0500

    Add twitter poster

--- index.js
+++ index.js
@@ -5,19 +5,23 @@ const themeManager = require('./src/themeManager')(config.themeTimeout)
 const historyManager = require('./src/historyManager')(config.historyFile, config.historyMax)
 const slackManager = require('./src/slackManager')(config.slackUrl)
 
+const twitterManager = require('./src/twitterManager')(
+  config.twitter.consumerKey, config.twitter.consumerSecret,
+  config.twitter.accessToken, config.twitter.accessTokenSecret)
+
 if (config.slackUrl === '') {
   console.error('Slack URL is not defined in config.js')
   process.exit(1)
 }
 historyManager.LoadHistory().then(function (history) {
   // Choose a theme
-  themeManager.chooseTheme(adjectives, themes, history).then(
-    // Post the theme to slack
-    slackManager.SlackPost
-  ).then(function (theme) {
+  themeManager.chooseTheme(adjectives, themes, history)
+    .then(slackManager.SlackPost)
+    .then(twitterManager.TwitterPost)
+    .then(function (theme) {
     // Update and save history
-    historyManager.AddHistory(theme, history).then(historyManager.SaveHistory(history))
-  })
+      historyManager.AddHistory(theme, history).then(historyManager.SaveHistory(history))
+    })
 }).catch(function (error) {
   // handle errors
   console.error(error)
---
+++ src/twitterManager.js
@@ -0,0 +1,24 @@
+const Twitter = require('twitter')
+
+module.exports = function (consumerKey, consumerSecret, accessToken, accessTokenSecret) {
+  return {
+    TwitterPost: function (theme) {
+      return new Promise(
+        function (resolve, reject) {
+          var client = new Twitter({
+            consumer_key: consumerKey,
+            consumer_secret: consumerSecret,
+            access_token: accessToken,
+            access_token_secret: accessTokenSecret
+          })
+          client.post('statuses/update', {status: 'Today\'s theme is ' + theme + '\n#artdailies'}, function (error, tweet, response) {
+            if (error) {
+              reject(new Error('Error: ' + error + ' response: ' + response + ' tweet: ' + tweet))
+            } else {
+              resolve(theme)
+            }
+          })
+        })
+    }
+  }
+}
