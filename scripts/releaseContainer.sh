85202a1fa09682b347c5a95b864e400a80196b86
Alex Hart <alex@hartcode.com>
2020-02-16 14:40:28 -0600

    build(kube.conf.json): fixes issues with the kube.conf.json format

--- scripts/releaseContainer.sh
+++ scripts/releaseContainer.sh
@@ -3,4 +3,3 @@ curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s htt
 chmod +x ./kubectl
 sed -i "s/(TRAVIS_COMMIT)/$TRAVIS_COMMIT/g" kube.conf.json
 ./kubectl apply -f kube.conf.json
-./kubectl rollout status deployment.v1.apps/challenge-of-the-day
