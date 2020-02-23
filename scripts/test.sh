c630d8f69f5b18d8564e03dd9c60b043d2acd078
Alex Hart <alex@hartcode.com>
2020-02-23 16:45:59 -0600

    test(test.sh): add missing env vars to build when running tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,6 +2,11 @@
 set -o nounset
 set -o errexit
 set -o pipefail
+export slackUrl="slackUrl"
+export twitterConsumerKey="ckey"
+export twitterConsumerSecret="csec"
+export twitterAccessToken="at"
+export twitterAccessTokenSecret="atsec"
 npm run lint
 npm run coverage
 npx codeclimate-test-reporter < coverage/lcov.info
