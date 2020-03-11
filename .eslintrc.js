33ea9686ee7b72dec5541453b39c4bf41a46268f
Alex Hart <alex@hartcode.com>
2020-03-11 10:46:12 -0500

    Update eslint with jest rules
    Adding eslint-plugin-jest eslint rules for better tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -290,7 +290,47 @@ module.exports = {
     'jsdoc/require-returns-check': 1, // Recommended
     'jsdoc/require-returns-description': 1, // Recommended
     'jsdoc/require-returns-type': 1, // Recommended
-    'jsdoc/valid-types': 1 // Recommended
+    'jsdoc/valid-types': 1, // Recommended
+    'jest/consistent-test-it': 'error',
+    'jest/expect-expect': 'error',
+    'jest/lowercase-name': 'error',
+    'jest/no-commented-out-tests': 'error',
+    'jest/no-disabled-tests': 'error',
+    'jest/no-expect-resolves': 'error',
+    'jest/no-export': 'error',
+    'jest/no-focused-tests': 'error',
+    'jest/no-hooks': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/no-if': 'error',
+    'jest/no-jest-import': 'error',
+    'jest/no-large-snapshots': 'error',
+    'jest/no-mocks-import': 'error',
+    'jest/no-jasmine-globals': 'error',
+    'jest/no-standalone-expect': 'error',
+    'jest/no-test-callback': 'error',
+    'jest/no-test-prefixes': 'error',
+    'jest/no-test-return-statement': 'error',
+    'jest/no-truthy-falsy': 'error',
+    'jest/no-try-expect': 'error',
+    'jest/valid-describe': 'error',
+    'jest/valid-expect': 'error',
+    'jest/valid-expect-in-promise': 'error',
+    'jest/valid-title': 'error',
+    'jest/no-alias-methods': 'error',
+    'jest/prefer-called-with': 'error',
+    'jest/prefer-expect-assertions': 'error',
+    'jest/prefer-to-be-null': 'error',
+    'jest/prefer-inline-snapshots': 'error',
+    'jest/prefer-spy-on': 'error',
+    'jest/prefer-hooks-on-top': 'error',
+    'jest/prefer-strict-equal': 'error',
+    'jest/prefer-to-be-undefined': 'error',
+    'jest/prefer-to-be-null': 'error',
+    'jest/prefer-to-contain': 'error',
+    'jest/prefer-to-have-length': 'error',
+    'jest/prefer-todo': 'error',
+    'jest/require-top-level-describe': 'error',
+    'jest/require-to-throw-message': 'error'
   },
   overrides: [
     {
