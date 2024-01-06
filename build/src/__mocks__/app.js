78332309fbd814b729465448e72f6b61bcd8e463
Alex Hart <alex@hartcode.com>
2024-01-06 16:04:16 -0600

    Ignore build directory

--- build/src/__mocks__/app.js
+++
@@ -1,1 +0,0 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:true});exports.default=void 0;let _DefaultExportValue=jest.fn();var _default=exports.default=_DefaultExportValue;
--- build/src/__mocks__/isomorphic-fetch.js
+++
@@ -1,1 +0,0 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:true});exports.default=void 0;let _DefaultExportValue=jest.fn().mockImplementation(()=>({statusCode:200}));var _default=exports.default=_DefaultExportValue;
